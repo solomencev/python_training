5c7760a719bbf807bc281e651b63f7b3d1f60376
solomencev <68597538+solomencev@users.noreply.github.com>
2020-11-11 18:50:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-import time
+from random import random
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.support.select import Select
 
 driver = WebDriver(executable_path=r'C://Python39/chromedriver.exe')
 driver.maximize_window()
@@ -21,3 +20,14 @@ def test_available_currencies():
     currencies = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/button/span').click()
     count = (len(driver.find_elements_by_class_name('select-list__item')))
     assert count == 16
+
+def test_the_matching_of_names_and_currencies():
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/button/span').click()
+
+    text = ''
+    textfinder1 = driver.find_elements_by_class_name('select-list__label')
+    for i in textfinder1:
+        text += i.text + ' '  # or whatever the way you want to concatenate your text
+    print(text)
+
+    driver.quit()
