5b8d277db704d5af020e3b34a96669ab2161a0b7
solomencev <68597538+solomencev@users.noreply.github.com>
2020-11-11 10:47:32 +0300

    Test check switch

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import time
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+driver = WebDriver(executable_path=r'C://Python39/chromedriver.exe')
+driver.maximize_window()
+driver.get('https://www.aviasales.ru/')
 
-def test():
-    driver = WebDriver(executable_path=r'C://Python39/chromedriver.exe')
-    driver.maximize_window()
-    driver.get('https://www.aviasales.ru/')
-    time.sleep(2)
-    driver = driver.find_element_by_class_name('_button_text_3oKFG').text
-    assert driver == 'Найти билеты'
+
+def test_name_of_submit():
+    submit = driver.find_element_by_class_name('_button_text_3oKFG').text
+    assert submit == 'Найти билеты'
+
+def test_switching_theme():
+    switch = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[1]/label/span/span').click()
+    assert switch == None
+    switch = driver.find_element_by_class_name('_theme_switch_checkbox_30psx').click()
+    assert switch == None
+
+
     print(None)
     ...
