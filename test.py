56dd013bc80f37394708150f431b8ab0229bb9da
solomencev <68597538+solomencev@users.noreply.github.com>
2020-11-12 08:50:55 +0300

    check names

--- test.py
+++ test.py
@@ -19,15 +19,24 @@ def test_switching_theme():
 def test_available_currencies():
     currencies = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/button/span').click()
     count = (len(driver.find_elements_by_class_name('select-list__item')))
-    assert count == 16
+    assert count >= 16
 
-def test_the_matching_of_names_and_currencies():
-    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/button/span').click()
+def test_names_services():
+    driver.find_element_by_class_name('navbar-services__icon').click()
+    map_of_prices = driver.find_element_by_xpath('//*[@title="Карта низких цен"]').text
+    assert map_of_prices == 'Карта низких цен'
 
-    text = ''
-    textfinder1 = driver.find_elements_by_class_name('select-list__label')
-    for i in textfinder1:
-        text += i.text + ' '  # or whatever the way you want to concatenate your text
-    print(text)
+    calendar_of_prices = driver.find_element_by_xpath('//*[@title="Календарь низких цен"]').text
+    assert calendar_of_prices == 'Календарь низких цен'
 
-    driver.quit()
+    bot = driver.find_element_by_xpath('//*[@title="Бот низких цен"]').text
+    assert bot == 'Бот низких цен'
+
+    articles_of_travel = driver.find_element_by_xpath('//*[@title="Статьи о путешествиях"]').text
+    assert articles_of_travel == 'Статьи о путешествиях'
+
+    specials = driver.find_element_by_xpath('//*[@title="Спецпредложения"]').text
+    assert specials == 'Спецпредложения'
+
+    business = driver.find_element_by_xpath('//*[@title="Для бизнеса"]').text
+    assert business == 'Для бизнеса\nB2B'
